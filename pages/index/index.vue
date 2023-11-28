d2fdf07b4b01c6bf9ae0585845ee4615552d2042
mao <1296193245@qq.com>
2023-11-28 08:42:16 +0800

    uniapp-page

---
+++ pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content">
+		<view class="data">
+			<text>{{val}}</text>
+		</view>
+		<button type="primary" @click="comunicationOff">结束监听</button>
+		<button type="primary" @click="start()">开始监听</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				val: 0
+			}
+		},
+		onLoad() {
+			setInterval(() => {
+				uni.$emit('add', {
+					data: 2
+				})
+			}, 1000)
+			uni.$on('add', this.add)
+		},
+		methods: {
+			comunicationOff() {
+				uni.$off('add', this.add)
+			},
+			add(e) {
+			
+				this.val += e.data
+			},
+			start() {
+				uni.$on('add', this.add)
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.data {
+		text-align: center;
+		line-height: 40px;
+		margin-top: 40px;
+	}
+
+	button {
+		width: 200px;
+		margin: 20px 0;
+	}
+</style>
